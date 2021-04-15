9c91d5f157ef0924663f8ce267b3b4d7646847ed
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-04-15 11:25:15 +0530

    Update codedamn.spec.js

--- cypress/integration/examples/codedamn.spec.js
+++ cypress/integration/examples/codedamn.spec.js
@@ -43,9 +43,13 @@ describe( 'Empty Test',() =>{
 
         cy.contains('Sign In').click()
 
+        cy.contains('Unable to authorize').should('not.exist')
+
         cy.get('[data-testid=username]').type('admin')
         cy.get('[data-testid=password]').type('admin')
 
         cy.get('[data-testid=login]').click()
+
+        cy.contains('Unable to authorize').should('exist')
     });
 })
