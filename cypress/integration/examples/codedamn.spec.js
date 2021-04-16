d048cde619d4c6bf6c108f90981ca6f1621b4f41
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-04-16 13:25:26 +0530

    Update codedamn.spec.js

--- cypress/integration/examples/codedamn.spec.js
+++ cypress/integration/examples/codedamn.spec.js
@@ -50,6 +50,29 @@ describe( 'Empty Test',() =>{
         cy.contains('My pending courses').should('exist')
     });
 
+    it('playground', () => {
+        cy.contains('Sign In').click()
+
+        cy.contains('Unable to authorize').should('not.exist')
+
+        cy.get('[data-testid=username]').type('blabs')
+        cy.get('[data-testid=password]').type('80216620Bi')
+
+        cy.get('[data-testid=login]').click()
+
+        // cy.contains('Unable to authorize').should('not.exist')
+
+        cy.contains('Unable to authorize',{timeout: 1*1000}).should('not.exist')
+
+        cy.contains('Playgrounds').click()
+        
+       
+        cy.get('div')
+        cy.debug()
+        
+    });
+
+
     it.only('playground', () => {
         cy.contains('Sign In').click()
 
@@ -62,11 +85,17 @@ describe( 'Empty Test',() =>{
 
         // cy.contains('Unable to authorize').should('not.exist')
 
+        cy.contains('Unable to authorize',{timeout: 1*1000}).should('not.exist')
+
         cy.contains('Playgrounds').click()
+
+        cy.contains('Raw HTML playground',{timeout:4*1000}).click()
+
+        cy.contains('Setting up the challenge',{timeout:100*1000}).should('not.exist')
         
-       
-        cy.get('div')
-        cy.debug()
+        cy.get('.xterm-helper-textarea',{timeout:10*1000}).type('{ctrl}{c}').type('touch test.js{enter}')
+
+        cy.contains('test.js').should('exist')
         
     });
 
