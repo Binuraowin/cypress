834e0434e56fa4db8565e44f66b1c344202cbeba
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-04-18 10:50:16 +0530

    Update saloon.spec.js

--- cypress/integration/examples/saloon.spec.js
+++ cypress/integration/examples/saloon.spec.js
@@ -3,12 +3,33 @@
 
 
 describe( 'Empty Test',() =>{
-  it('test one', () => {
+  it.only('test one', () => {
       
-      cy.visit('http://localhost:3000/appointment')
+      cy.visit('http://localhost:3000',{timeout: 5*1000})
 
-      cy.contains('Appointment').click()
+      cy.get('.appointment',{timeout: 4*1000}).click()
 
-     
+      cy.url().should('include','/appointment')
+
+      // cy.contains('Make an Appointment').should('exist')
+
+  });
+
+  it('description', () => {
+
+    cy.visit('http://localhost:3000/appointment')
+    cy.get('.description').type('description')
+
+    cy.get('.email').type('email@email.com')
+
+    cy.get('.name').type('binura owin')
+
+    cy.get('.date').type('2021-04-16')
+
+    cy.get('.phone').type('0452288529')
+
+    cy.get('.makeappointment').click()
+
+    // cy.contains('Please fill out this field.').should('exist')
   });
 })
